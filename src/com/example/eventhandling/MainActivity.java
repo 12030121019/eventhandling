5153ec4d9b2202b68a3dfc1a9e93cc4b7815d863
manishvatsal <manish.v2011@gmail.com>
2014-12-16 09:49:32 +0530

    OnClick

--- src/com/example/eventhandling/MainActivity.java
+++ src/com/example/eventhandling/MainActivity.java
@@ -1,15 +1,38 @@
 package com.example.eventhandling;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+	private Button click;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+       click= (Button) findViewById(R.id.btnClick);
+       OnClickListener listener = new OnClickListener() {
+		
+		@Override
+		public void onClick(View arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+	};
+       
+       
+       click.setText("Proceed");
+       click.setText(getResources().getString(R.string.caption));
+       click.setTextColor(getResources().getColor(R.color.dk_blue));
+       click.setTextColor(Color.GREEN);
+       click.setBackgroundColor(Color.YELLOW);
+       click.setTextSize((float)12.3);
     }
 
 
